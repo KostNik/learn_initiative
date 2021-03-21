2a87d37ba697fec6cd8f5bae1ee29ca57a4bef53
Mykyta Kostiuk <kostiuk.mykyta5@gmail.com>
2021-03-21 17:04:58 +0200

    Add all products endpoint

--- src/main/java/com/learn/initiative/store/controller/ProductController.java
+++ src/main/java/com/learn/initiative/store/controller/ProductController.java
@@ -4,10 +4,12 @@ import com.learn.initiative.store.model.dto.Product;
 import com.learn.initiative.store.repository.ProductsRepository;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("v1/products")
 @AllArgsConstructor
@@ -18,6 +20,7 @@ public class ProductController {
     @GetMapping("/all")
     @ApiOperation("Get all products")
     public Iterable<Product> allProducts() {
+        log.info("GET ALL PRODUCTS");
         return productsRepository.findAll();
     }
 
