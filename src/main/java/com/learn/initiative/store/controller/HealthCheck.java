94c00bf70e5345f69f5e544cb2a895b1d4ae29a3
Mykyta Kostiuk <kostiuk.mykyta5@gmail.com>
2021-03-21 15:17:36 +0200

    Fix build

--- src/main/java/com/learn/initiative/store/controller/HealthCheck.java
+++ src/main/java/com/learn/initiative/store/controller/HealthCheck.java
@@ -1,7 +1,6 @@
 package com.learn.initiative.store.controller;
 
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +11,10 @@ import java.time.LocalDateTime;
 @RequestMapping("v1")
 public class HealthCheck {
 
-    @Value("${rdb.username}")
-    private String username;
-    @Value("${rdb.password}")
-    private String password;
-    @Value("${rdb.url}")
-    private String url;
-
     @GetMapping("/ping")
     @ApiOperation("Health check the Application")
     public String ping() {
-        return "PONG: [>" + LocalDateTime.now() + "<]" +
-                " username : " + username +
-                " password:" + password +
-                " host : " + url;
+        return "PONG: [>" + LocalDateTime.now() + "<]";
     }
 
 }
